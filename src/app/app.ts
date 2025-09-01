8f8dbabf32883554dcc03be1ddec8b1ef4e6b5a4
Kingsly Bulong <bulongkingsly@gmail.com>
2025-09-02 06:36:35 +0800

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterModule, RouterLink, RouterLinkActive],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true
 })
 export class App {
   protected readonly title = signal('Portfolio');
